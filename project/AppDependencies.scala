9ac023506d70ca4b60f72beef0e87bd88bbf1e9e
pratikrana06 <63359242+pratikrana06@users.noreply.github.com>
2021-11-11 16:25:37 +0000

    [DEP-2275][PR]:Library and Plugin Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,17 +1,14 @@
-import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.0.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.16.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.1.2"                 % Test,
+    "org.scalatest"           %% "scalatest"                % "3.1.4"                 % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % Test,
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.0.0"                 % Test
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % Test
   )
 }
