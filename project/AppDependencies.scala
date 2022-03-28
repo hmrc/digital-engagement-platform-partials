179e47b6a46198ea34d471894a924475c6e8eccb
pratikrana06 <63359242+pratikrana06@users.noreply.github.com>
2022-03-28 16:23:07 +0100

    [DEP-2403][PR]:Scalatest upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,8 +7,8 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.2.3"                 % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.35.10"               % Test,
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % Test
+    "org.scalatest"           %% "scalatest"                % "3.2.11"  % Test,
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.62.2"  % Test,
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"   % Test
   )
 }
