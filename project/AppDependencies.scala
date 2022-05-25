cfc9f1ff6859e0c1ad8f0c23df51d395b586cad3
Pratik Rana <63359242+pratikrana06@users.noreply.github.com>
2022-05-25 09:47:21 +0100

    [DEP-3048][PR]:Library upgrades (#36)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.21.0"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.24.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"                % "3.2.11"  % Test,
+    "org.scalatest"           %% "scalatest"                % "3.2.12"  % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.62.2"  % Test,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"   % Test
   )
